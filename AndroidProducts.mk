cda3a5a06bd6995f26190e13b20b001917eb149c
DqrKn3zz <rear097@gmail.com>
2021-02-17 16:32:01 +0100

    ginkgo: Bringup AOSiP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 ArrowOS
+# Copyright (C) 2021 Android Open Source Illusion Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/aosip_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    aosip_ginkgo-user \
+    aosip_ginkgo-userdebug \
+    aosip_ginkgo-eng
